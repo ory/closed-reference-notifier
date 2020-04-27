282dfe77741e3765a7a9efd11ab4afc110214b69
zepatrik <11patti1@gmx.de>
2020-04-27 21:24:47 +0200

    feat: add basic functionality

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { GitHub } from '@actions/github'
+import core from '@actions/core'
+import fs from 'fs'
+import walkdir from "walkdir"
+
+const referenceRegex = /github\.com\/([a-zA-Z\d-]+)\/([a-zA-Z\d.-_]+)\/(pull|issues)\/(\d+)/gm
+const issueLabel = 'closed reference'
+
+const gitHubClient = new GitHub(core.getInput('token'))
+
+// const issueTitle = (upstreamReference: string) =>
+//   `upstream reference closed: ${upstreamReference}`
+//
+// const createIssue = (upstreamReference: string) => {
+//   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/', 2)
+//
+//   return gitHubClient.issues.create({
+//     owner,
+//     repo,
+//     title: issueTitle(upstreamReference),
+//     labels: [issueLabel]
+//   })
+// }
+
+walkdir.async('.', { return_object: true })
+  .then((files) =>
+    Object.entries(files).forEach(([path, stats]: [string, fs.Stats]) =>
+      stats.isDirectory() || fs.readFile(path, (err, data) => {
+        if (err) {
+          console.log(err)
+          process.exit(1)
+          return
+        }
+
+        for (let match of data.toString().matchAll(referenceRegex)) {
+          const [reference, owner, repo, type, id] = match
+          gitHubClient.issues
+            .get({
+              owner,
+              repo,
+              issue_number: parseInt(id)
+            })
+            .then((issue) => {
+              if (issue.data.state == 'closed') {
+                console.log('found closed reference:', reference)
+              }
+            })
+            .catch(err => {
+              console.log(err)
+              process.exit(1)
+            })
+        }
+      })
+    )
+  )
