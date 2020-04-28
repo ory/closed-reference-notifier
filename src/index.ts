df8755a48bcd4b989ab018e91a49849e75e890c0
zepatrik <11patti1@gmx.de>
2020-04-28 12:36:09 +0200

    u

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import { GitHub } from '@actions/github'
 import core from '@actions/core'
 import fs from 'fs'
-import walkdir from "walkdir"
+import walkdir from 'walkdir'
 
 const referenceRegex = /github\.com\/([a-zA-Z\d-]+)\/([a-zA-Z\d.-_]+)\/(pull|issues)\/(\d+)/gm
 const issueLabel = 'closed reference'
 
 const gitHubClient = new GitHub(core.getInput('token'))
+const [thisOwner, thisRepo] = process.env.GITHUB_REPOSITORY.split('/', 2)
 
-// const issueTitle = (upstreamReference: string) =>
-//   `upstream reference closed: ${upstreamReference}`
-//
+const issueTitle = (upstreamReference: string) =>
+  `upstream reference closed: ${upstreamReference}`
+
 // const createIssue = (upstreamReference: string) => {
-//   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/', 2)
+//
 //
 //   return gitHubClient.issues.create({
 //     owner,
@@ -22,10 +23,11 @@ const gitHubClient = new GitHub(core.getInput('token'))
 //   })
 // }
 
-walkdir.async('.', { return_object: true })
-  .then((files) =>
-    Object.entries(files).forEach(([path, stats]: [string, fs.Stats]) =>
-      stats.isDirectory() || fs.readFile(path, (err, data) => {
+walkdir.async('.', { return_object: true }).then((files) =>
+  Object.entries(files).forEach(
+    ([path, stats]: [string, fs.Stats]) =>
+      stats.isDirectory() ||
+      fs.readFile(path, (err, data) => {
         if (err) {
           console.log(err)
           process.exit(1)
@@ -43,13 +45,23 @@ walkdir.async('.', { return_object: true })
             .then((issue) => {
               if (issue.data.state == 'closed') {
                 console.log('found closed reference:', reference)
+                gitHubClient.issues
+                  .list({
+                    labels: issueLabel
+                  })
+                  .then((issues) => {
+                    issues.data.find(
+                      (issue) => issue.title === issueTitle(reference)
+                    ) ||
+                      console.log('closed reference without issue:', reference)
+                  })
               }
             })
-            .catch(err => {
+            .catch((err) => {
               console.log(err)
               process.exit(1)
             })
         }
       })
-    )
   )
+)
