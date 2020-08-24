9c34ec59dbbf43a525d3f32424a5ca4f8a5ef9cd
zepatrik <zepatrik@users.noreply.github.com>
2020-08-24 16:10:13 +0000

    autogen: build bundles

--- distCli/index.js
+++ distCli/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+-e #!/usr/bin/env node
 module.exports =
 /******/ (function(modules, runtime) { // webpackBootstrap
 /******/ 	"use strict";
@@ -5497,7 +5497,7 @@ if (
 ) {
   /* eslint no-control-regex: "off" */
   const makePosix = str => /^\\\?\/.test(str)
-  || /["<>| -]+/u.test(str)
+  || /["<>|\u0000-\u001F]+/u.test(str)
     ? str
     : str.replace(/\/g, '/')
 
@@ -5853,7 +5853,7 @@ class Blob {
 		this[BUFFER] = Buffer.concat(buffers);
 
 		let type = options && options.type !== undefined && String(options.type).toLowerCase();
-		if (type && !/[^ -~]/.test(type)) {
+		if (type && !/[^\u0020-\u007E]/.test(type)) {
 			this[TYPE] = type;
 		}
 	}
@@ -6259,12 +6259,12 @@ function convertBody(buffer, headers) {
 
 	// html5
 	if (!res && str) {
-		res = /<meta.+?charset=(['"])(.+?)\1/i.exec(str);
+		res = /<meta.+?charset=(['"])(.+?)/i.exec(str);
 	}
 
 	// html4
 	if (!res && str) {
-		res = /<meta[\s]+?http-equiv=(['"])content-type\1[\s]+?content=(['"])(.+?)\2/i.exec(str);
+		res = /<meta[\s]+?http-equiv=(['"])content-type[\s]+?content=(['"])(.+?)/i.exec(str);
 
 		if (res) {
 			res = /charset=(.*)/i.exec(res.pop());
@@ -6273,7 +6273,7 @@ function convertBody(buffer, headers) {
 
 	// xml
 	if (!res && str) {
-		res = /<\?xml.+?encoding=(['"])(.+?)\1/i.exec(str);
+		res = /<\?xml.+?encoding=(['"])(.+?)/i.exec(str);
 	}
 
 	// found charset
@@ -6462,7 +6462,7 @@ Body.Promise = global.Promise;
  */
 
 const invalidTokenRegex = /[^\^_`a-zA-Z\-0-9!#$%&'*+.|~]/;
-const invalidHeaderCharRegex = /[^	 -~€-ÿ]/;
+const invalidHeaderCharRegex = /[^	\x20-\x7e\x80-\xff]/;
 
 function validateName(name) {
 	name = `${name}`;
@@ -23406,10 +23406,10 @@ var funcTag = '[object Function]',
     symbolTag = '[object Symbol]';
 
 /** Used to match property names within property paths. */
-var reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\]|\.)*?\1)\]/,
+var reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!)[^\]|\.)*?)\]/,
     reIsPlainProp = /^\w*$/,
     reLeadingDot = /^\./,
-    rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\]|\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g;
+    rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!)[^\]|\.)*?))\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g;
 
 /**
  * Used to match `RegExp`
@@ -24593,10 +24593,10 @@ var funcTag = '[object Function]',
     symbolTag = '[object Symbol]';
 
 /** Used to match property names within property paths. */
-var reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\]|\.)*?\1)\]/,
+var reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!)[^\]|\.)*?)\]/,
     reIsPlainProp = /^\w*$/,
     reLeadingDot = /^\./,
-    rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\]|\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g;
+    rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!)[^\]|\.)*?))\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g;
 
 /**
  * Used to match `RegExp`
